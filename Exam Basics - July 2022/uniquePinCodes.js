4dbcfc5e2c7dfe36d6b3505806ef3b8d57f785fa
Hristo92 <78822762+Hristo92@users.noreply.github.com>
2022-09-25 18:09:10 +0100

    Update the solution
    Some updates on this task and solution.

--- Exam Basics - July 2022/uniquePinCodes.js
+++ Exam Basics - July 2022/uniquePinCodes.js
@@ -1,54 +1,17 @@
-function uniquePinCodes(num1, num2, num3){
+function uniquePinCodes(num1, num2, num3) {
 
-        let counter = 0;
+    for (let i = 2; i <= num1; i += 2) {
 
-        for (let i = 0; i < num1; i++) {
-            if (num1 % 2 === 0) {
-                counter += i;
-            }
-            for (let j = 0; j < num2; j++) {
-                if (num2 >= 2 && num2 <= 7) {
-                    counter += j;
+        for (let j = 2; j <= num2; j++) {
+
+            for (let p = 2; p <= num3; p += 2) {
+                if (p % 2 === 0) {
+                    console.log(i, j, p,);
                 }
-                for (let p = num3;p < num3;p++) {
-                    if (num3 % 2 === 0) {
-                        counter += i;                
-                }                
-            }    
-            console.log(i);
-        
+            }
         }
-
-
-
-
-   //let index = 0;
-
-   //let firstNumber = Number(input[index]);
-   //index++;
-
-   //let secondNumber = Number(input[index]);
-   //index++;
-
-   //let thirdNumber = Number(input[index]);
-   //index++;
-
-   //let myNumber = 0;
-
-   //for (let i = 0; i < 9; i++) {
-
-   //    if(firstNumber && thirdNumber % 2 === 0){
-   //        myNumber++;
-   //    }
-   //    for (let j = 0; j < i; j++) {
-   //        if(thirdNumber >= 2 && thirdNumber <= 7){
-   //            myNumber++;
-   //        }
-   //    }
-   //    
-   //    
-   //}
-   //console.log(myNumber)
-
+    }
 }
-uniquePinCodes("3", "5", "5")
+
+uniquePinCodes(2, 2, 2)
+
